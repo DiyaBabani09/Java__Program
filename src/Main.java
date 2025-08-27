0a6a27351ee68758f838d03f5095678a53f0f1d2
DiyaBabani09 <diyababani03@gmail.com>
2025-08-27 19:12:53 +0530

    DZ-2 : added sample print statement

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,8 @@ public class Main {
 
         Demo demo = new Demo();
         demo.hello();
+
+        System.out.println("hello");
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.printf("Hello and welcome!");
